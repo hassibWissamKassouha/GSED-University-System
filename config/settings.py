e48b52fc27d8c08992231719d3f29a59a1225cbf
antonAzar <azaranton24@gmail.com>
2026-03-21 03:09:44 +0300

    install django-cors-headers and update requirements

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # My apps
-    'django-cors-headers',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'core',
